e46192f623a471a743111916e6de726d15613361
AliAl-nasser <ali.al-nasser1@hotmail.com>
2026-09-05 14:13:44 +0200

    Implement retrieval of stored products

--- GetProducts.cs
+++ GetProducts.cs
@@ -1,3 +1,5 @@
+using Advania.CandidateTest.Repositories;
+using Azure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -7,17 +9,49 @@ namespace Advania.CandidateTest;
 
 public class GetProducts
 {
+    private readonly ProductRepository _repository;
     private readonly ILogger<GetProducts> _logger;
 
-    public GetProducts(ILogger<GetProducts> logger)
+    public GetProducts(
+        ProductRepository repository,
+        ILogger<GetProducts> logger)
     {
+        _repository = repository;
         _logger = logger;
     }
 
     [Function("GetProducts")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    public async Task<IActionResult> Run(
+        [HttpTrigger(
+            AuthorizationLevel.Anonymous,
+            "get",
+            Route = "products")] HttpRequest req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+        try
+        {
+            var products = await _repository.GetAllAsync(
+                req.HttpContext.RequestAborted);
+
+            var response = products.Select(product => new
+            {
+                id = product.RowKey,
+                name = product.Name,
+                quantity = product.Quantity
+            }).ToList();
+
+            return new OkObjectResult(response);
+        }
+        catch (RequestFailedException ex)
+        {
+            _logger.LogError(ex, "Failed to retrieve products.");
+
+            return new ObjectResult(new
+            {
+                error = "Products could not be retrieved."
+            })
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+        }
     }
 }
